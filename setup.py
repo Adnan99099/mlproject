c544305c1d4d1291f41bc0edf149a86377e2caf4
Muhammad Adnan <adnan.ned98@gmail.com>
2025-09-09 13:10:27 -0700

    requirnemet

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mlproject",
+    version="0.1",
+    author='Muhammad_Adnan',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn'],
+)
